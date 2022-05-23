e416aa918c1b494532e3e535c5971bbe11b50628
Ackeraa <ggaoshanxun@gmail.com>
2022-05-23 20:39:17 +0800

    bug not fixed

--- main.py
+++ main.py
@@ -26,18 +26,53 @@ class Main(ThreeDScene):
 
         self.play(Rotating(cube, radians=2*PI, run_time=2))
 
+        '''
         self.begin_ambient_camera_rotation(rate=0.5)
         self.wait(3)
-        '''
+
 
 class Test(ThreeDScene):
     def construct(self):
-        cube = Cube((1, 2, 3))
         cube = RubiksCube()
 
-        self.move_camera(phi=60*DEGREES, theta=160*DEGREES)
-        #self.renderer.camera.frame_center = cube.get_center()
+        self.move_camera(phi=60*DEGREES, theta=30*DEGREES)
+        self.renderer.camera.frame_center = cube.get_center()
+        axes = ThreeDAxes()
         self.add(cube)
-        self.wait()
+        self.add(axes)
+        vg = VGroup(*cube[:, 2, :].flatten())
+        vg1 = VGroup(*cube[:, 0, :].flatten())
+        vg2 = VGroup(*cube[:, 1, :].flatten())
+        self.bring_to_back(vg2)
+        self.bring_to_back(vg)
+        self.play(Rotating(vg, radians=PI/2, axis=UP, about_point=cube[1][2][1].get_center()))
 
+        self.interactive_embed()
+
+
+class Test2(ThreeDScene):
+    def construct(self):
+
+        cube = Cube((1,2,3))
+        cube.set(fill_color=BLUE, stroke_color=BLACK, fill_opacity=1)
+        cube.set_fill(BLUE, 1.0)
+        cube1 = cube.copy().next_to(cube, UP, buff=0)
+        self.move_camera(phi=60*DEGREES, theta=-30*DEGREES)
+        self.renderer.camera.frame_center = cube.get_center()
+        axes = ThreeDAxes()
+        self.add(cube)
+        self.bring_to_back(cube[4])
+        self.play(FadeIn(cube))
+        self.interactive_embed()
+
+class Test3(ThreeDScene):
+    def construct(self):
+        self.move_3d_camera(phi=60*DEGREES, theta=-30*DEGREES)
+        self.begin_ambient_camera_rotation()
+        face = Square(stroke_color=RED, fill_color=BLUE, fill_opacity=1, shade_in_3d=True)
+        face.apply_matrix(z_to_vector(RIGHT))
+        face1 = face.copy().scale(0.5).move_to(LEFT)
+        self.bring_to_back(face1)
+        self.add(face, face1)
+        self.interactive_embed()
 
--- rubiks_cube.py
+++ rubiks_cube.py
@@ -1,29 +1,36 @@
 from manim import *
 from cube import Cube 
+import numpy as np
 
 COLORS = [WHITE, "#B90000", "#009B48", "#FFD500", "#FF5900", "#0045AD"]
 class RubiksCube(VGroup):
     
     def __init__(self, dim=3, cube_side_length=1, colors=COLORS):
+        # IN, RIGHT, UP ---> X, Y, Z
         self.dim = dim
         self.cube_side_length = cube_side_length
+        self.cubies = np.ndarray((dim, dim, dim), dtype=Cube)
         
-        super().__init__(fill_color=BLUE, fill_opacity=1, stroke_color=BLACK, stroke_width=3)
-
-
+        super().__init__(fill_color=BLUE, fill_opacity=1, stroke_color=BLACK, stroke_width=4)
+        self.generate_points()
 
     def generate_points(self):
+        x_vg = VGroup()
         for x in range(self.dim):
             y_vg = VGroup()
             for y in range(self.dim):
                 z_vg = VGroup()
                 for z in range(self.dim):
                     cube = Cube((x, y, z), side_length=self.cube_side_length)
+                    self.add(cube)
+                    self.cubies[x][y][z] = cube
                     z_vg.add(cube)
-                z_vg.arrange(IN, buff=0)
+                z_vg.arrange(OUT, buff=0)
                 y_vg.add(z_vg)
-            y_vg.arrange(RIGHT, buff=0)
-            self.add(y_vg)
-        self.arrange(UP, buff=0)
+            y_vg.arrange(UP, buff=0)
+            x_vg.add(y_vg)
+        x_vg.arrange(RIGHT, buff=0)
 
+    def __getitem__(self, key):
+        return self.cubies[key]
 
